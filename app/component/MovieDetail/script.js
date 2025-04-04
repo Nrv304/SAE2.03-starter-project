d2fd8f6ad7b5503054a08c3f5a8923f7ca475ea8
Nrv304 <owentr04@gmail.com>
2025-04-05 00:24:50 +0200

    CSS iteration 3

--- app/component/MovieDetail/script.js
+++ app/component/MovieDetail/script.js
@@ -6,7 +6,7 @@ let MovieDetail = {};
 // Formate les détails d'un film avec le template
 MovieDetail.format = function (film) {
   let html = template;
-  html = html.replace("{{movietTitle}}", film.name);
+  html = html.replace("{{movieTitle}}", film.name);
   html = html.replace("{{movieDirector}}", film.director);
   html = html.replace("{{movieReleaseYear}}", film.year);
   html = html.replace("{{movieDescription}}", film.description);
